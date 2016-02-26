8350029daf6a2db55eb70ea5d32864d4a912327d
Karl Brown <karl@thekarlbrown.com>
2016-02-25 21:01:02 -0500

    Initial Layout of MagicTracker site

---
+++ Website/about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>About</title>
+<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" href="css/materialize.css" media="screen,projection"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+<script src="js/jquery-2.2.1.min.js"></script>
+<script src="js/mtgtracker.js"></script>
+<script src="js/materialize.js"></script>
+<nav>
+    <div class="light-blue accent-3 nav-wrapper">
+      <a href="" class="brand-logo">MtgTracker</a>
+	  <a href="" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="light-blue accent-3 right hide-on-med-and-down">
+        <li><a href="track.php">Track A Card</a></li>
+        <li><a href="bySet.php">Cards By Set</a></li>
+		<li><a href="alerts.php">Your Alerts</a></li>
+        <li><a href="profile.php">Profile</a></li>
+		<li class="active"><a href="about.php">About</a></li>
+      </ul>
+	  <ul class="light-blue accent-3 side-nav" id="mobile-demo">
+        <li><a href="track.php">Track A Card</a></li>
+        <li><a href="bySet.php">Cards By Set</a></li>
+		<li><a href="alerts.php">Your Alerts</a></li>
+        <li><a href="profile.php">Profile</a></li>
+		<li class="active"><a href="about.php">About</a></li>
+      </ul>
+    </div>
+  </nav>
+</body>
+</html>
