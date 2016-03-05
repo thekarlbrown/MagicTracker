86a06bd6f7571749be6f1c1fea017630615d461c
Karl Brown <karl@thekarlbrown.com>
2016-03-05 16:21:56 -0500

    Updated for Autocomplete

--- Tools/json-processor.py
+++ Tools/json-processor.py
@@ -6,6 +6,7 @@ with open('AllSets.json','r', encoding='utf-8') as data_file:
     data = json.load(data_file)
 
 shortenedJSON = "["
+autocompleteJSON = "["
 for set in data:
     print ("-- " + data[set]["name"] + " --") 
     shortenedJSON += "{\"name\": \"" + data[set]["name"] + "\", \"cards\": ["
@@ -14,11 +15,17 @@ for set in data:
         if multiverseid is None:
             multiverseid=0;
         print (card["name"] + " " + str(multiverseid))
+        autocompleteJSON += "{\"set\": \"" + data[set]["name"] + "\", \"multiverseid\": " + str(multiverseid) + ", \"name\": \"" + card["name"] + "\"},"
         shortenedJSON += "{\"multiverseid\": " + str(multiverseid) + ", \"name\": \"" + card["name"] + "\"},"
     shortenedJSON = shortenedJSON[:-1]
     shortenedJSON += "]},"
+autocompleteJSON = autocompleteJSON[:-1]
+autocompleteJSON += "]"
 shortenedJSON = shortenedJSON[:-1]
 shortenedJSON += "]"
 with codecs.open("AllSets-short.json",'w','utf-8') as output_file:
     output_file.write(shortenedJSON)
+    output_file.close
+with codecs.open("AllSets-autocomplete.json",'w','utf-8') as output_file:
+    output_file.write(autocompleteJSON)
     output_file.close
