5e6693812cc925030c2193d782f5e3bb4c138340
Karl Brown <karl@thekarlbrown.com>
2016-03-04 21:36:44 -0500

    Improvements to Your Alerts

--- Website/alerts.php
+++ Website/alerts.php
@@ -31,10 +31,13 @@
       </ul>
     </div>
   </nav>
+  
 <div class="row">
- <div class="col s12 m5 l4"><p style="font-weight: bold">Evil eye of orms by gore</p></div>
- <div class="col s12 m4 l4"><p style="font-style: italic">Champions of Kamigawa Kamigawa</p></div>
- <div class="col s12 m3 l4"><p>210.21 TCGHigh 125.12 TCGMid 101.23 TCGLow</p></div>
+ <div class="col s12"><p style="text-align: center; text-decoration: underline; font-size: 30px; font-weight: bold; margin: 5px 0px 0px 0px">Your Alerts</p></div>
+ 
+ <div class="col s12 m5 l4"><p style="font-weight: bold;text-align: center;">Anafenza, Kin-Tree Spirit</p></div>
+ <div class="col s12 m4 l4"><p style="font-style: italic;text-align: center;">Dragons of Tarkir</p></div>
+ <div class="col s12 m3 l4"><p><span style="color: red;">0.40 TCGLow</span><span style="color: blue;"> 0.66 TCGMid</span><span style="color: green;"> 3.30 TCGHigh </span></p></div>
  <div class="input-field col s6 m3 l2">
           <input id="target_high" type="text" onkeypress='return (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46'>
           <label for="target_high">Price Alert High</label>
@@ -45,7 +48,7 @@
   <option value="1" selected>TCG Mid</option>
   <option value="2">TCG High</option>
  </select>
- <label>Price Guide</label>
+ <label>Price Guide High</label>
  </div>
  <div class="input-field col s6 m3 l2">
           <input id="target_low" type="text" onkeypress='return (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46'>
@@ -57,15 +60,52 @@
   <option value="1" selected>TCG Mid</option>
   <option value="2">TCG High</option>
  </select>
- <label>Price Guide</label>
+ <label>Price Guide Low</label>
  </div>
- <div class="col s8 m6 l2 switch" style="text-align: center"><label>
+ <div class="col s8 m6 l3 switch" style="text-align: center; padding-top: 20px;"><label>
       Deactivated
       <input type="checkbox">
       <span class="lever"></span>
       Activated
     </label></div>
- <div class="col s4 m6 l2" style="text-align: center"><a class="waves-effect waves-light btn red lighten-1">Delete</a></div>
+ <div class="col s4 m6 l1" style="text-align: center; padding-top: 11px;"><img src="img/delete-icon.png" alt="Delete Alert"/></div>
+ 
+ <div class="col s12" style="width: 94%; margin-left: 2%;padding: 10px 0 10px 0px;"><div style="border-bottom: 4px solid gainsboro;"></div></div>
+ 
+ <div class="col s12 m5 l4"><p style="font-weight: bold;text-align: center;">Tarmogoyf</p></div>
+ <div class="col s12 m4 l4"><p style="font-style: italic;text-align: center;">Future Sight</p></div>
+ <div class="col s12 m3 l4"><p><span style="color: red;">149.99 TCGLow</span><span style="color: blue;"> 189.99 TCGMid</span><span style="color: green;"> 222.1 TCGHigh </span></p></div>
+ <div class="input-field col s6 m3 l2">
+          <input id="target_high" type="text" onkeypress='return (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46'>
+          <label for="target_high">Price Alert High</label>
+        </div>
+ <div class="input-field col s6 m3 l2">
+ <select id="highmidlow-high">
+  <option value="0">TCG Low</option>
+  <option value="1" selected>TCG Mid</option>
+  <option value="2">TCG High</option>
+ </select>
+ <label>Price Guide High</label>
+ </div>
+ <div class="input-field col s6 m3 l2">
+          <input id="target_low" type="text" onkeypress='return (event.charCode >= 48 && event.charCode <= 57) || event.charCode == 46'>
+          <label for="target_low">Price Alert Low</label>
+        </div>
+ <div class="input-field col s6 m3 l2">
+ <select id="highmidlow-low">
+  <option value="0">TCG Low</option>
+  <option value="1" selected>TCG Mid</option>
+  <option value="2">TCG High</option>
+ </select>
+ <label>Price Guide Low</label>
+ </div>
+ <div class="col s8 m6 l3 switch" style="text-align: center; padding-top: 20px;"><label>
+      Deactivated
+      <input type="checkbox">
+      <span class="lever"></span>
+      Activated
+    </label></div>
+ <div class="col s4 m6 l1" style="text-align: center; padding-top: 11px;"><img src="img/delete-icon.png" alt="Delete Alert"/></div>
 </div>
 </body>
 </html>
